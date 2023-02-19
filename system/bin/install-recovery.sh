33107c5d566114a66df43a5945e1d501758dd623
root <root@MainDesk>
2023-02-19 14:04:07 +0100

    [update to V01.02.0400]

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
-if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:67108864:463a0afe804a295ab63cdb1cde5f4f2036456c3d; then
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:67108864:e849ad38ae7e29c9188c49c7711ab3ac8f93f939; then
   applypatch  \
           --patch /system/recovery-from-boot.p \
-          --source EMMC:/dev/block/bootdevice/by-name/boot:67108864:cbe5bfd4f5e3594513f692798c66be78639f253e \
-          --target EMMC:/dev/block/bootdevice/by-name/recovery:67108864:463a0afe804a295ab63cdb1cde5f4f2036456c3d && \
+          --source EMMC:/dev/block/bootdevice/by-name/boot:67108864:8ce4396b3ffdcd167e253826a818ca1c757009de \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery:67108864:e849ad38ae7e29c9188c49c7711ab3ac8f93f939 && \
       log -t recovery "Installing new recovery image: succeeded" || \
       log -t recovery "Installing new recovery image: failed"
 else
